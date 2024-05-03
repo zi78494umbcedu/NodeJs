f00a35ca16282f6c1a84eec56da63a5b9b2f5f82
Ashwin Gupta <zi78494@umbc.edu>
2024-05-03 13:33:19 -0400

    initial_commit

---
+++ script.js
@@ -0,0 +1,39 @@
+//Fundamentals of Javascript
+//arrays and objects
+//functions return
+//async js coding
+
+var arr = [1,2,3,4];
+arr.forEach(function(val){
+    return val+12;
+})
+var ans = arr.map(function(val){
+    return 12;
+});
+var fil = arr.filter(function(val){
+    if(val>3)return true;
+})
+var find = arr.find(function(val){
+    if(val==3)return val;
+})
+console.log(arr.indexOf(2));
+console.log(find);
+console.log(fil);
+console.log(ans);
+console.log(arr);
+
+var obj = {
+    name:"Ashwin"
+}
+Object.freeze(obj)
+
+obj.name = "Kemchi"
+console.log(obj.name);
+console.log(obj['name']);
+console.log(obj)
+
+console.log(typeof function(){})
+function abc(a, b, c){}
+console.log(abc.length)
+console.dir(abc)
+
